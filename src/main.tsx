0576d6ff31780b25c81c976679c09ab295439ba4
Randy Truong <rtruong60@gmail.com>
2023-11-05 08:31:59 -0500

    Fixing mui theme

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,24 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const queryClient = new QueryClient();
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
